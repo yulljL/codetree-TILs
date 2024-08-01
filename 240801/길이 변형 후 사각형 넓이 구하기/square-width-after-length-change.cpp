2c545d5e7eb1c61eaba6f71e10992d9df04307be
yulljL <lms4994@naver.com>
2024-08-01 23:37:43 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    a += 8;
+    b *= 3;
+    
+    cout << a << endl << b << endl << a*b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
