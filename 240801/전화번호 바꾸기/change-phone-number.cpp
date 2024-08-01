8779788f7502940dc04a5d2dbfbb689eadb0057b
yulljL <lms4994@naver.com>
2024-08-01 20:40:16 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int o, xxxx, yyyy;
+    char c;
+
+    cin >> o >> c >> yyyy >> c >> xxxx;
+
+    cout << "0" << o << c << xxxx << c << yyyy;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
