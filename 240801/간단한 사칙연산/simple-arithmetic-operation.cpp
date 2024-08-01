659fd164f6c8598eeabcac4526246b0025e3eb9b
yulljL <lms4994@naver.com>
2024-08-01 23:32:59 +0900

    Title: 간단한 사칙연산, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b;
+    
+    cin >> a >> b;
+    cout << a+b << endl << a-b << endl << a / b << endl << a;// 여기에 코드를 작성해주세요.
+    return 0;
+}
