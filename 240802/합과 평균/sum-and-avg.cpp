d6d6f980fc0002f48a00b9607423bcf30b474d0a
yulljL <lms4994@naver.com>
2024-08-02 18:25:10 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/합과 평균/sum-and-avg.cpp
+++ 240802/합과 평균/sum-and-avg.cpp
@@ -6,7 +6,8 @@ int main() {
     int a, b;
 
     cin >> a >> b;
-    
+    cout << fixed;
+    cout.precision(1);
     cout << a + b << " " << (double)(a + b) / 2;
     // 여기에 코드를 작성해주세요.
     return 0;
