fdd526644af011e8913dc5c9e7c9687f0bbce634
yulljL <lms4994@naver.com>
2024-08-02 15:04:05 +0900

    Title: 2개의 정수를 서로 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/2개의 정수를 서로 더하기/add-two-integers-each-other.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >>b;
+    
+    a += b;
+    b += a;
+    
+    cout << a << " " << b;// 여기에 코드를 작성해주세요.
+    return 0;
+}
