a0b6b7731bba87bd10db5e03dc6c1c84461eba5a
yulljL <lms4994@naver.com>
2024-08-02 18:23:29 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+
+    cin >> a >> b;
+    
+    cout << a + b << ' ' << (double)(a + b) / 2;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
