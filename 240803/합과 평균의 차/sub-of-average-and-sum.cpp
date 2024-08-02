aa797febdc05f023a281568391e3126757dcf3de
yulljL <lms4994@naver.com>
2024-08-03 01:47:59 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    int s = a+b+c;
+    int m = (a+b+c)/3;
+    
+    cout << s << endl << m << endl << s - m;// 여기에 코드를 작성해주세요.
+    return 0;
+}
