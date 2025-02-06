c70a27c257cb269e0ef1a0590a7d6f20fdecea3d
yulljL <lms4994@naver.com>
2025-02-06 17:46:43 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250206/동전이 있는 위치/Where-coins-are-located.cpp
+++ 250206/동전이 있는 위치/Where-coins-are-located.cpp
@@ -2,18 +2,28 @@
 using namespace std;
 
 int main() {
-    int n, m , r, c;
+    int n, m;
     cin >> n >> m;
-    int arr[n][n] = {0};
 
-    for(int i = 0; i < m; i++) {
-        int r, c;
-        cin >> r >> c;
-        arr[r - 1][c - 1] = 1;
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            arr[i][j] = 0;
+        }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+        int r, c;
+        cin >> r >> c;
+
+        if (r >= 1 && r <= n && c >= 1 && c <= n) {
+            arr[r - 1][c - 1] = 1; 
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
