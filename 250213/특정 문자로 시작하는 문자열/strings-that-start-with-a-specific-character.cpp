214ad707a0347d13895fe24f5a9bd54cf3ba0083
yulljL <lms4994@naver.com>
2025-02-13 16:55:32 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250213/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
+++ 250213/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 #include <string>
+#include <iomanip> 
+
 using namespace std;
 
 int main() {
-    int n;
+    int n; 
     cin >> n;
-    string str[20];
 
-    for(int i = 0; i < n; i++) {
-        cin >> str[i];
+    string strings[20];
+    for (int i = 0; i < n; ++i) {
+        cin >> strings[i];
     }
-    
-    char a;
-    cin >> a;
-    int cnt = 0;
-    int len_all = 0;
 
-    for(int i = 0; i < n; i++) {
-        int len = str[i].length();
-        len_all += len;
-        if(str[i][0] == a) {
-            cnt++;
+    char targetChar; 
+    cin >> targetChar;
+
+    int count = 0; 
+    int totalLength = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (strings[i][0] == targetChar) { 
+            ++count;
+            totalLength += strings[i].length();
         }
     }
-    double avg = (len_all) / n;
 
-    cout << fixed;
-    cout.precision(2);
-    cout << cnt << " " << avg;
+    // 평균 길이 계산 (소수점 둘째 자리까지)
+    double averageLength = static_cast<double>(totalLength) / count;
+
+    // 결과 출력
+    cout << count << " ";
+    cout << fixed << setprecision(2) << averageLength << endl;
 
     return 0;
 }
+
