5ef3ef630b052520d7ffec58d4e5827459898179
yulljL <lms4994@naver.com>
2025-02-13 14:09:33 +0900

    Title: 단어로 구분하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250213/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str[10];
+
+    for(int i = 0; i < 10; i++) {
+        cin >> str[i];
+    }
+
+    for(int i = 9; i >= 0; i--) {
+        cout << str[i] << endl;
+    }
+
+    return 0;
+}
